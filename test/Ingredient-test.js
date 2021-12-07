e2531f938745a7008a2d736172d647f59793af9b
Daniel O'Connell <danieloconnell.maker@gmail.com>
2021-12-07 14:55:18 -0800

    Run linter on ingredient test

--- test/Ingredient-test.js
+++ test/Ingredient-test.js
@@ -4,31 +4,30 @@ import Ingredient from '../src/classes/Ingredient';
 import testData from '../src/data/testing-data/ingredients-test-data';
 
 describe('Ingredient', () => {
-	let ingredientOne;
-	
+  let ingredientOne;
 
-	beforeEach(() => {
-		ingredientOne = new Ingredient(testData.ingredientsData[0])
-	})
+  beforeEach(() => {
+    ingredientOne = new Ingredient(testData.ingredientsData[0]);
+  });
 
-	it('should be a function', () => {
-		expect(Ingredient).is.a('function')
-	})
-	
-	it('should be an instantiation of Ingredient', () => {
-		expect(ingredientOne).to.be.an.instanceOf(Ingredient)
-	})
+  it('should be a function', () => {
+    expect(Ingredient).is.a('function');
+  });
 
-	it('should have an ID', () => {
-		expect(ingredientOne.id).to.deep.equal(20081)
-	})
+  it('should be an instantiation of Ingredient', () => {
+    expect(ingredientOne).to.be.an.instanceOf(Ingredient);
+  });
 
-	it('should have a name', () => {
-		expect(ingredientOne.name).to.deep.equal("wheat flour")
-	})
+  it('should have an ID', () => {
+    expect(ingredientOne.id).to.deep.equal(20081);
+  });
 
-	it('should contain an estimated cost in cents', () => {
-		expect(ingredientOne.estimatedCostInCents).to.deep.equal(142)
-	})
+  it('should have a name', () => {
+    expect(ingredientOne.name).to.deep.equal("wheat flour");
+  });
 
-})
+  it('should contain an estimated cost in cents', () => {
+    expect(ingredientOne.estimatedCostInCents).to.deep.equal(142);
+  });
+
+});
