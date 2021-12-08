7b4f09623cfc2089e15d09a840ba37882a426ced
Alex Fritz <alexfritzcoach@gmail.comm>
2021-12-08 13:38:45 -0800

    Refactor small errors

--- src/data/testing-data/recipe-test-data.js
+++ src/data/testing-data/recipe-test-data.js
@@ -8,24 +8,28 @@ const recipeData = [
         "quantity": {
           "amount": 1.5,
           "unit": "c"
+        }
     	},
     	{
         "id": 18372,
         "quantity": {
           "amount": 0.5,
           "unit": "tsp"
+        }
     	},
     	{
         "id": 1123,
         "quantity": {
           "amount": 1,
           "unit": "large"
+        }
     	},
     	{
         "id": 19335,
         "quantity": {
           "amount": 0.5,
           "unit": "c"
+        }
     	}
     ],
     "instructions": [
@@ -53,24 +57,28 @@ const recipeData = [
         "quantity": {
           "amount": 1.5,
           "unit": "c"
+        }
     	},
     	{
         "id": 18372,
         "quantity": {
           "amount": 0.5,
           "unit": "tsp"
+        }
     	},
     	{
         "id": 1123,
         "quantity": {
           "amount": 1,
           "unit": "large"
+        }
     	},
     	{
         "id": 19335,
         "quantity": {
           "amount": 0.5,
           "unit": "c"
+        }
     	}
     ],
     "instructions": [
@@ -96,24 +104,28 @@ const recipeData = [
         "quantity": {
           "amount": 1.5,
           "unit": "c"
+        }
     	},
     	{
         "id": 18372,
         "quantity": {
           "amount": 0.5,
           "unit": "tsp"
+        }
     	},
     	{
         "id": 1123,
         "quantity": {
           "amount": 1,
           "unit": "large"
+        }
     	},
     	{
         "id": 19335,
         "quantity": {
           "amount": 0.5,
           "unit": "c"
+        }
     	}
     ],
     "instructions": [
