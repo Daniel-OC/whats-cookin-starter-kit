c9ac4df4725cc51c5b89a3332b7fde619e649df1
Ethan Tweitmann <ethantweitmann@gmail.com>
2021-12-07 16:09:28 -0800

    Create recipe test data file

---
+++ src/data/testing-data/recipe-test-data.js
@@ -0,0 +1,120 @@
+const recipeData = [
+  {
+    "id": 595736,
+    "image": "https://spoonacular.com/recipeImages/595736-556x370.jpg",
+    "ingredients": [
+      {
+    		"id": 20081,
+    		"name": "wheat flour",
+    		"estimatedCostInCents": 142
+    	},
+    	{
+    		"id": 18372,
+    		"name": "bicarbonate of soda",
+    		"estimatedCostInCents": 582
+    	},
+    	{
+    		"id": 1123,
+    		"name": "eggs",
+    		"estimatedCostInCents": 472
+    	},
+    	{
+    		"id": 19335,
+    		"name": "sucrose",
+    		"estimatedCostInCents": 902
+    	}
+    ],
+    "instructions": [
+      {
+        "instruction": "Instructions for 1st Recipe",
+        "number": 1
+      }
+    ],
+    "name": "Loaded Chocolate Chip Pudding Cookie Cups",
+    "tags": [
+      "antipasti",
+      "starter",
+      "snack",
+      "appetizer",
+      "antipasto",
+      "hor d'oeuvre"
+    ]
+  },
+  {
+    "id": 593223,
+    "image": "https://spoonacular.com/recipeImages/595736-556x370.jpg",
+    "ingredients": [
+      {
+    		"id": 20081,
+    		"name": "wheat flour",
+    		"estimatedCostInCents": 142
+    	},
+    	{
+    		"id": 18372,
+    		"name": "bicarbonate of soda",
+    		"estimatedCostInCents": 582
+    	},
+    	{
+    		"id": 1123,
+    		"name": "eggs",
+    		"estimatedCostInCents": 472
+    	},
+    	{
+    		"id": 19335,
+    		"name": "sucrose",
+    		"estimatedCostInCents": 902
+    	}
+    ],
+    "instructions": [
+      {
+        "instruction": "Instructions for 2nd test recipe",
+        "number": 1
+      }
+    ],
+    "name": "Loaded Chocolate Chip Pudding Cookie Cups",
+    "tags": [
+      "lunch",
+      "main course",
+      "main dish",
+      "dinner"
+    ]
+  },
+  {
+    "id": 599385,
+    "image": "https://spoonacular.com/recipeImages/595736-556x370.jpg",
+    "ingredients": [
+      {
+    		"id": 20081,
+    		"name": "wheat flour",
+    		"estimatedCostInCents": 142
+    	},
+    	{
+    		"id": 18372,
+    		"name": "bicarbonate of soda",
+    		"estimatedCostInCents": 582
+    	},
+    	{
+    		"id": 1123,
+    		"name": "eggs",
+    		"estimatedCostInCents": 472
+    	},
+    	{
+    		"id": 19335,
+    		"name": "sucrose",
+    		"estimatedCostInCents": 902
+    	}
+    ],
+    "instructions": [
+      {
+        "instruction": "Instructions for 3rd test recipe",
+        "number": 1
+      }
+    ],
+    "name": "Loaded Chocolate Chip Pudding Cookie Cups",
+    "tags": [
+      "sauce"
+    ]
+  }
+];
+
+export default {recipeData}
