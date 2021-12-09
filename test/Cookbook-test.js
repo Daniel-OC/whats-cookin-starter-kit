a70604107ede0fd8bc75fd26a35d648ffb62f4f5
Daniel O'Connell <danieloconnell.maker@gmail.com>
2021-12-08 16:40:31 -0800

    Rename recipe repository tto be cookbook

---
+++ test/Cookbook-test.js
@@ -0,0 +1,30 @@
+import { expect } from 'chai';
+import Cookbook from '../src/classes/Cookbook';
+import ingredientTestData from '../src/data/testing-data/ingredients-test-data'
+import recipeTestData from '../src/data/testing-data/recipe-test-data'
+
+
+describe('Recipe', () => {
+  let cookbook
+
+  beforeEach(() => {
+
+  })
+  
+  it('Should be a function', () => {
+    expect(Cookbook).to.be.a('function');
+  });
+
+  it('should be a function')
+  
+  it('should be an instance of RecipeRepository')
+
+  it('should be able to filter by 1 tag')
+
+  it('should be able to filter by 2 or more tags')
+
+  it('should be able to filter by name')
+
+  it('should be able to filter by ingredient')
+
+})
