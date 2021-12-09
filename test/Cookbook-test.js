99331475f42a2b694bd0210e0be28c5c027311f0
Ethan Tweitmann <ethantweitmann@gmail.com>
2021-12-09 14:37:17 -0800

    Refactor Cookbook test to import ingredient test data

--- test/Cookbook-test.js
+++ test/Cookbook-test.js
@@ -2,25 +2,30 @@ import { expect } from 'chai';
 import Cookbook from '../src/classes/Cookbook';
 import Recipe from '../src/classes/Recipe';
 import recipeTestData from '../src/data/testing-data/recipe-test-data';
+import ingredientTestData from '../src/data/testing-data/ingredients-test-data';
 
 
-describe('Recipe', () => {
+
+describe('Cookbook', () => {
   let recipes;
   let cookbook;
+  let recipeData;
+  let ingredients;
 
   beforeEach(() => {
-    let recipeData = recipeTestData.recipeData;
+    ingredients = ingredientTestData.ingredientsData;
+    recipeData = recipeTestData.recipeData;
     recipes = recipeData.reduce((sum, recipe) => {
       sum.push(new Recipe(recipe));
       return sum;
     }, []);
-    cookbook = new Cookbook(recipes);
+    cookbook = new Cookbook(recipes, ingredients);
   });
-  
+
   it('Should be a function', () => {
     expect(Cookbook).to.be.a('function');
   });
-  
+
   it('should be an instance of Cookbook', () => {
     expect(cookbook).to.be.an.instanceOf(Cookbook);
   });
@@ -31,23 +36,23 @@ describe('Recipe', () => {
   });
 
   it('should be able to filter by 1 tag', () => {
-    cookbook.filterByTag('snack');
+    cookbook.filterByTag(['snack']);
     expect(cookbook.currentRecipes.length).to.deep.equal(2);
   });
 
   it('should be able to filter by 2 or more tags', () => {
-    cookbook.filterByTag('snack', 'lunch');
-    expect(cookbook.currentRecipes.length).to.deep.equal(1);
+    cookbook.filterByTag(['snack', 'lunch']);
+    expect(cookbook.currentRecipes.length).to.deep.equal(2);
   });
 
   it('should be able to filter by name', () => {
-    cookbook.filterByKeyword('Loaded');
+    cookbook.filterByRecipeName('Loaded Chocolate');
     expect(cookbook.currentRecipes.length).to.deep.equal(1);
   });
 
   it('should be able to filter by ingredient', () => {
-    cookbook.filterByKeyword('eggs');
-    expect(cookbook.currentRecipes.length).to.deep.equal(3); 
+    cookbook.filterByIngredient('eggs');
+    expect(cookbook.currentRecipes.length).to.deep.equal(3);
   });
 
   it('should reset search or filter terms', () => {
