dd64e527b83f162c85bd6d3eb88a46c662dc6a5e
Ethan Tweitmann <ethantweitmann@gmail.com>
2021-12-09 14:36:27 -0800

    Create initial Cookbook class build

--- src/classes/Cookbook.js
+++ src/classes/Cookbook.js
@@ -1,7 +1,25 @@
 class Cookbook {
-  constructor() {
+  constructor(recipes, ingredients) {
+    this.recipes = recipes;
+    this.ingredients = ingredients;
+  }
 
-    // One class to get you started!
+  filterByTag(searchTags) {
+    this.currentRecipes = this.recipes.filter(recipe => {
+      return recipe.tags.some(tag => searchTags.includes(tag));
+    });
+  }
+
+  filterByRecipeName(keywords) {
+    this.currentRecipes = this.recipes.filter(recipe => {
+      return recipe.name.toLowerCase().includes(keywords.toLowerCase());
+    });
+  }
+
+  filterByIngredient(keywords) {
+    this.currentRecipes = this.recipes.filter(recipe => {
+      return recipe.getIngredientNames(this.ingredients).includes(keywords.toLowerCase());
+    })
   }
 }
 
