4771f582f5b578862945ba2840b76a4169c34aa5
Ethan Tweitmann <ethantweitmann@gmail.com>
2021-12-09 14:55:54 -0800

    Add clearFilter method

--- src/classes/Cookbook.js
+++ src/classes/Cookbook.js
@@ -19,7 +19,11 @@ class Cookbook {
   filterByIngredient(keywords) {
     this.currentRecipes = this.recipes.filter(recipe => {
       return recipe.getIngredientNames(this.ingredients).includes(keywords.toLowerCase());
-    })
+    });
+  }
+
+  clearFilter() {
+    this.currentRecipes = this.recipes;
   }
 }
 
