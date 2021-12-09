abd3377deb45cff95e023b8eafe4f3c21ca8771a
Daniel O'Connell <danieloconnell.maker@gmail.com>
2021-12-08 18:08:09 -0800

    Run linter and fix minor errors accordingly

--- test/Cookbook-test.js
+++ test/Cookbook-test.js
@@ -1,8 +1,7 @@
 import { expect } from 'chai';
 import Cookbook from '../src/classes/Cookbook';
 import Recipe from '../src/classes/Recipe';
-import ingredientTestData from '../src/data/testing-data/ingredients-test-data'
-import recipeTestData from '../src/data/testing-data/recipe-test-data'
+import recipeTestData from '../src/data/testing-data/recipe-test-data';
 
 
 describe('Recipe', () => {
@@ -15,47 +14,46 @@ describe('Recipe', () => {
       sum.push(new Recipe(recipe));
       return sum;
     }, []);
-    cookbook = new Cookbook(recipes)
-  })
+    cookbook = new Cookbook(recipes);
+  });
   
   it('Should be a function', () => {
     expect(Cookbook).to.be.a('function');
   });
   
   it('should be an instance of Cookbook', () => {
-    expect(cookbook).to.be.an.instanceOf(Cookbook)
-  })
+    expect(cookbook).to.be.an.instanceOf(Cookbook);
+  });
 
   it('should store recipes', () => {
     expect(cookbook.recipes).to.be.an('array');
-    expect(cookbook.recipes[0]).to.be.an.instanceOf(Recipe)
-  })
+    expect(cookbook.recipes[0]).to.be.an.instanceOf(Recipe);
+  });
 
   it('should be able to filter by 1 tag', () => {
-    cookbook.filterByTag('snack')
-    expect(cookbook.currentRecipes.length).to.deep.equal(2)
-  })
+    cookbook.filterByTag('snack');
+    expect(cookbook.currentRecipes.length).to.deep.equal(2);
+  });
 
   it('should be able to filter by 2 or more tags', () => {
-    cookbook.filterByTag('snack','lunch')
-    expect(cookbook.currentRecipes.length).to.deep.equal(1)
-  })
+    cookbook.filterByTag('snack', 'lunch');
+    expect(cookbook.currentRecipes.length).to.deep.equal(1);
+  });
 
   it('should be able to filter by name', () => {
-    cookbook.filterByKeyword('Loaded')
-    expect(cookbook.currentRecipes.length).to.deep.equal(1)
-  })
+    cookbook.filterByKeyword('Loaded');
+    expect(cookbook.currentRecipes.length).to.deep.equal(1);
+  });
 
   it('should be able to filter by ingredient', () => {
-    cookbook.filterByKeyword('eggs')
-    expect(cookbook.currentRecipes.length).to.deep.equal(3)  
-  })
+    cookbook.filterByKeyword('eggs');
+    expect(cookbook.currentRecipes.length).to.deep.equal(3); 
+  });
 
   it('should reset search or filter terms', () => {
-    cookbook.filterByKeyword('eggs')
-    expect(cookbook.currentRecipes.length).to.deep.equal(3) 
-    cookbook.clearFilter()
-    expect(cookbook.currentRecipes).to.deep.equal(cookbook.recipes)
-  })
-
-})
+    cookbook.filterByKeyword('eggs');
+    expect(cookbook.currentRecipes.length).to.deep.equal(3);
+    cookbook.clearFilter();
+    expect(cookbook.currentRecipes).to.deep.equal(cookbook.recipes);
+  });
+});
