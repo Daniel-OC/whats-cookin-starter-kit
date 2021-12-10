5fa9774c25393d1375ee8aa29d7bac15274e2d58
Daniel O'Connell <danieloconnell.maker@gmail.com>
2021-12-09 17:48:16 -0800

    Add tests for favoriteRecipes and MealPlan

--- test/User-test.js
+++ test/User-test.js
@@ -4,15 +4,19 @@ import User from '../src/classes/User';
 // import recipeTestData from '../src/data/testing-data/recipe-test-data';
 import ingredientTestData from '../src/data/testing-data/ingredients-test-data';
 import userTestData from '../src/data/testing-data/user-test-data';
+import recipeTestData from '../src/data/testing-data/recipe-test-data'
+import Recipe from '../src/classes/Recipe';
 
 
 describe('User', () => {
   let ingredients
   let users 
   let testUser;
-  let favorites
+  let favorites;
+  let recipes
   
   beforeEach(() => {
+    recipes = recipeTestData.recipeData
     ingredients = ingredientTestData.ingredientsData
     users = userTestData.userData
     favorites = new Cookbook(ingredients)
@@ -41,28 +45,39 @@ describe('User', () => {
   });
 
   it('should be able to keep track of favorite recipes', () => {
-
+    expect(testUser.favoriteRecipes).to.be.instanceOf(Cookbook)
   });
 
   it('should be able to keep track of meals to cook', () => {
-
-
+    
   });
 
   it('should be able to add favorite recipes', () => {
-
+    let newRecipe = new Recipe(recipes[0])
+    testUser.addFavoriteRecipe(newRecipe)
+    expect(testUser.favoriteRecipes.currentRecipes.length).to.deep.equal(1)
   });
 
   it('should be able to remove favorite recipes', () => {
-
+    let newRecipe = new Recipe(recipes[0])
+    testUser.addFavoriteRecipe(newRecipe)
+    expect(testUser.favoriteRecipes.currentRecipes.length).to.deep.equal(1)
+    testUser.removeFavoriteRecipe(newRecipe)
+    expect(testUser.favoriteRecipes.currentRecipes.length).to.deep.equal(0)
   });
 
   it('should be able to add recipes to mealplan', () => {
-
+    let newRecipe = new Recipe(recipes[0])
+    testUser.addToMealPlan(newRecipe)
+    expect(testUser.mealPlan.length).to.deep.equal(1)
   });
 
   it('should be able to remove recipes from mealplan', () => {
-
+    let newRecipe = new Recipe(recipes[0])
+    testUser.addToMealPlan(newRecipe)
+    expect(testUser.mealPlan.length).to.deep.equal(1)
+    testUser.removeFromMealPlan(newRecipe)
+    expect(testUser.mealPlan.length).to.deep.equal(0)
   });
 
 });
