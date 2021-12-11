d78fe402f21599dbca3927dd91a4e3e6f769274e
Ethan Tweitmann <ethantweitmann@gmail.com>
2021-12-10 16:00:30 -0800

    Add ids for querySelectors and import data to scripts

--- src/scripts.js
+++ src/scripts.js
@@ -1,7 +1,24 @@
 import './styles.css';
 import apiCalls from './apiCalls';
+import Cookbook from '../src/classes/Cookbook';
+import User from '../src/classes/User';
+import Recipe from '../src/classes/Recipe';
+import Ingredient from '../src/classes/Ingredient';
+import userData from './data/users';
+import recipeData from './data/recipes';
+import ingredientData from './data/ingredients';
 
+const ingredients = ingredientData.ingredientsData;
+const recipes = recipeData.recipeData;
+const users = userData.usersData; 
 
-
-
-console.log('Hello world');
+const logoSection = document.querySelector('#logoSection');
+const searchBar = document.querySelector('#searchBar');
+const searchButton = document.querySelector('#searchButton');
+const homeButton = document.querySelector('#homeButton');
+const yourFavsButton = document.querySelector('#yourFavsButton');
+const bigModal = document.querySelector('#bigModal');
+const mainDisplay = document.querySelector('#mainDisplay');
+const filterButton = document.querySelector('#filterButton');
+const mealPlanButton = document.querySelector('#mealPlanButton');
+const sideBarModal = document.querySelector('#sideBarModal');
