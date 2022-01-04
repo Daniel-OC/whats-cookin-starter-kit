b9dbbe1cf6b25b36d50de04989e05e6df2acb1c6
Ethan Tweitmann <ethantweitmann@gmail.com>
2022-01-04 13:18:17 -0800

    Refactor filterByRecipeName method to be more inclusive

--- src/classes/Cookbook.js
+++ src/classes/Cookbook.js
@@ -13,20 +13,9 @@ class Cookbook {
     });
   }
 
-  // filterByRecipeName() {
-  //   if (this.keywords.length) {
-  //     this.currentRecipes = this.currentRecipes.filter(recipe => {
-  //       return recipe.name.toLowerCase().includes(this.keywords.join(" "));
-  //     });
-  //   }
-  //   else {
-  //     this.clearFilter();
-  //   }
-  // }
-
   filterByRecipeName(recipeList) {
     this.currentRecipes = recipeList.filter(recipe => {
-      return recipe.name.toLowerCase().includes(this.keywords.join(" "));
+      return recipe.name.toLowerCase().split(' ').some(word => this.keywords.includes(word));
     });
   }
 
@@ -36,18 +25,6 @@ class Cookbook {
       return this.keywords.every(keyword => ingredientNamesArray.includes(keyword));
     });
   }
-
-  // filterByIngredient() {
-  //   if (this.keywords.length) {
-  //     this.currentRecipes = this.recipes.filter(recipe => {
-  //       let ingredientNamesArray = recipe.getIngredientNames(this.ingredients).flat();
-  //       return this.keywords.every(keyword => ingredientNamesArray.includes(keyword));
-  //     });
-  //   } else {
-  //     this.clearFilter();
-  //   }
-  // }
-
 
   clearFilter(recipeList) {
     this.currentRecipes = recipeList || this.recipes;
