66893bb2b5e2e53974dc0ff244bce19d667b5f07
Alex Fritz <alexfritzcoach@gmail.comm>
2022-01-07 18:25:45 -0800

    Refactor POST request function

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,3 +1,4 @@
+
 const recipeCalls =
   fetch('http://localhost:3001/api/v1/recipes')
     .then(response => response.json());
@@ -14,15 +15,16 @@ const pantryCalls = (user, ing, amt) => {
   return fetch('http://localhost:3001/api/v1/users', {
     method: 'POST',
     body: JSON.stringify({
-      "userID": user.id,
-      "ingredientID": ing.id,
-      "ingredientModification": amt
+      userID: parseInt(user.id),
+      ingredientID: parseInt(ing),
+      ingredientModification: parseInt(amt)
     }),
     headers: {
       'Content-Type': 'application/json'
     }
   })
 }
+
 
 
 module.exports = {recipeCalls, ingredientCalls, userCalls, pantryCalls};
