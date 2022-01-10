9b7d84e84b9efaf46a3a80638e1aaed3ff92e733
Ethan Tweitmann <ethantweitmann@gmail.com>
2022-01-09 16:38:11 -0800

    Remove checkPantryInventory method as it is no longer necessary

--- src/classes/Pantry.js
+++ src/classes/Pantry.js
@@ -3,41 +3,28 @@ class Pantry {
     this.ingredients = ingredients;
   }
 
-  checkPantryInventory(recipe) {
-    return recipe.ingredients.reduce((bool, ingredient) => {
-      let matchedIng = this.ingredients.find(userIng => userIng.ingredient === ingredient.id)
-      if (matchedIng && (matchedIng.amount >= ingredient.quantity.amount)) {
-
-        bool = true;
-      } else {
-        bool = false;
-      }
-      return bool;
-    }, false)
-  }
-
   listNeededIngredients(recipe) {
     return recipe.ingredients.reduce((neededIngredients, ingredient) => {
       let matchedIng = this.ingredients.find(userIng => userIng.ingredient === ingredient.id)
       if (matchedIng === undefined) {
-        let missingIng = { 
-          id: ingredient.id, 
-          quantity: { 
-            amount: ingredient.quantity.amount, 
+        let missingIng = {
+          id: ingredient.id,
+          quantity: {
+            amount: ingredient.quantity.amount,
             unit: ingredient.quantity.unit
           }
         }
         neededIngredients.push(missingIng);
       } else if (matchedIng && (matchedIng.amount < ingredient.quantity.amount)) {
-        let neededIng = { 
-          id: matchedIng.ingredient, 
-          quantity: { 
-            amount: ingredient.quantity.amount - matchedIng.amount, 
+        let neededIng = {
+          id: matchedIng.ingredient,
+          quantity: {
+            amount: ingredient.quantity.amount - matchedIng.amount,
             unit: ingredient.quantity.unit
           }
         }
         neededIngredients.push(neededIng);
-      } 
+      }
       return neededIngredients;
     }, [])
   }
